9a020217beaeb7ab44ca0a7e62e8500ad69ce80c
ryosuke <s21m079vv@ous.jp>
2024-10-22 18:43:28 +0900

    create nodes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "points#new"
+  root "points#index"
   resources :points, only: [:index, :create]
 end
